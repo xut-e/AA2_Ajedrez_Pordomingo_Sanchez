f01a544d9366c09505f9cffdc78c3987af75248b
xut <xut.workmail@gmail.com>
2025-05-04 15:32:30 +0200

    Castle
    Castle not working properly yet

--- AA2_Ajedrez_Sanchez_Pordomingo/moves.h
+++ AA2_Ajedrez_Sanchez_Pordomingo/moves.h
@@ -7,6 +7,6 @@ bool menuMovimiento();
 
 bool movePiece(char chessboard[BOARD_SIZE][BOARD_SIZE], std::vector<Pieces>& listPiecePos, int jugador);
 
-void movimiento(std::vector<Pieces>& listPiecePos, int idPieza, int jugador, bool& movimientoValido, Position& casillaFinal, bool& comer);
+void movimiento(std::vector<Pieces>& listPiecePos, int idPieza, int jugador, bool& movimientoValido, Position& casillaFinal, bool& comer, bool& enroque);
 
-void cambiarPosicion(int idPieza, Position casillaFinal, std::vector<Pieces>& listPiecePos);
+void cambiarPosicion(int idPieza, Position casillaFinal, std::vector<Pieces>& listPiecePos, bool& enroque);
